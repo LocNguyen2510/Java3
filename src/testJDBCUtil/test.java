0c331896d444f8948468cb1066aa9532e1dff724
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-12-17 23:28:21 +0700

    Learn JDBC SQL with Java

--- src/testJDBCUtil/test.java
+++ src/testJDBCUtil/test.java
@@ -6,9 +6,10 @@ import database.JDBCUltil;
 
 public class test {
 	public static void main(String[] args) {
-
+		// bước 1: tạo kết nối
 		Connection connection = JDBCUltil.getConnection();
 		JDBCUltil.printInfo(connection);
+//		bước 5: ngắt kết nối
 		JDBCUltil.closeConnection(connection);
 	}
 }
