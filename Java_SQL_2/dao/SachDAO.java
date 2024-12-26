5a536220d55aa4b8fbe043c954c23c4b7f8145c8
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2024-12-26 21:28:53 +0700

    Create Insert, Update, Delete class Sach (SQL) with Java

--- Java_SQL_2/dao/SachDAO.java
+++ Java_SQL_2/dao/SachDAO.java
@@ -43,13 +43,60 @@ public class SachDAO implements DAOInterface<Sach> {
 
 	@Override
 	public int update(Sach t) {
-		return 0;
+		int ketQua = 0;
+		try {
+			// Bước 1: tạo kết nối đến CSDL
+			Connection con = JDBCUtil1.getConnection();
+			// Buowcs 2: tạo ra đối tượng statement
+			Statement st = con.createStatement();
+			// Bước 3: thực thi câu lệnh SQL
+			String sql = "UPDATE sach" + " SET " + " tenSach='" + t.getTenSach() + "'" + ", giaBan=" + t.getGiaBan()
+					+ ", namXuatBan=" + t.getNamXuatBan() + " WHERE id='" + t.getId() + "\'";
+			System.out.println(sql);
+
+			ketQua = st.executeUpdate(sql);
+
+//			Bước 4:
+			System.out.println("Bạn đã thực thi: " + sql);
+			System.out.println("Có " + ketQua + " dòng bị thay đổi !!");
+
+//			Bước 5:
+			JDBCUtil1.closeConnection(con);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return ketQua;
 	}
 
 	@Override
 	public int delete(Sach t) {
-		// TODO Auto-generated method stub
-		return 0;
+		int ketQua = 0;
+		try {
+			// Bước 1: tạo kết nối đến CSDL
+			Connection con = JDBCUtil1.getConnection();
+			// Buowcs 2: tạo ra đối tượng statement
+			Statement st = con.createStatement();
+			// Bước 3: thực thi câu lệnh SQL
+			String sql = "DELETE from sach" + " WHERE id='" + t.getId() + "'";
+			System.out.println(sql);
+
+			ketQua = st.executeUpdate(sql);
+
+//			Bước 4:
+			System.out.println("Bạn đã thực thi: " + sql);
+			System.out.println("Có " + ketQua + " dòng bị thay đổi !!");
+
+//			Bước 5:
+			JDBCUtil1.closeConnection(con);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return ketQua;
+
 	}
 
 	@Override
--- Java_SQL_2/testJDBCUtil/testSachDAO.java
+++ Java_SQL_2/testJDBCUtil/testSachDAO.java
@@ -5,8 +5,23 @@ import model.Sach;
 
 public class testSachDAO {
 	public static void main(String[] args) {
-		Sach sach1 = new Sach("LTJV", "Lập trình Java", 50000, 2024);
-		Sach sach2 = new Sach("LTC", "Lập trình C", 75000, 2025);
-		SachDAO.getInstance().add(sach2);
+//		Test insert: 
+//		Sach sach1 = new Sach("LTJV", "Lập trình Java", 50000, 2024);
+//		Sach sach2 = new Sach("LTC", "Lập trình C", 75000, 2025);
+//		Sach sach3 = new Sach("LTC#", "Lập trình C#", 35000, 2022);
+//		SachDAO.getInstance().add(sach3);
+		for (int i = 0; i < 20; i++) {
+			Sach sach = new Sach("LT" + i, "Lập trình " + i, 20000, 2012);
+			SachDAO.getInstance().add(sach);
+		}
+//		Test update
+		Sach sach1 = new Sach("LTJV", "Lập trình Java", 23000, 2010);
+		SachDAO.getInstance().update(sach1);
+
+//		Test delete
+		for (int i = 2; i < 5; i++) {
+			Sach sach = new Sach("LT" + i, "Lập trình " + i, 20000, 2012);
+			SachDAO.getInstance().delete(sach);
+		}
 	}
 }
